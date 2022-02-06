ad367ffe06d93408c87d5dda457dcdd6499aca99
Ubuntu <ubuntu@ip-172-26-11-212.ap-southeast-1.compute.internal>
2022-02-06 07:40:34 +0000

    task

---
+++ panscan/app/models/task.rb
@@ -0,0 +1,58 @@
+class Task < ActiveRecord::Base
+    def self.create_task(name,code)
+      task = Task.new
+      task.status = "open"
+      task.code = code
+      task.name = name
+      task.save
+    end
+    
+    def self.take_task(runner)
+      task = Task.where(status:"open").first
+      task.status = "run"
+      task.runner = runner
+      task.output = ""
+      task.save
+  
+      task.log("#{Time.now} runner #{runner} take task #{task.id} : #{task.name}\n")
+      return task
+    end
+    
+    def log(str)
+      self.output = self.output + str
+      self.save
+    end
+  
+    
+    class Runner
+      def initialize(task)
+        @_task = task
+      end
+      def _log(str)
+        @_task.log(str)
+      end
+      def _run(code)
+        eval(code,binding)
+      end
+    end
+    
+    def run
+      runner = Runner.new(self)
+      self.log("#{Time.now} == begin run ==\n")
+      begin
+        runner._run(self.code)
+      rescue => error
+        self.log "Exception Class: #{ error.class.name }\n"
+        self.log "Exception Message: #{ error.message }\n"
+        self.log "Exception Backtrace:\n#{ error.backtrace.join("\n") }\n"
+        self.log("#{Time.now} == abort run ==\n")
+        self.status = "abort"
+        self.save
+      else
+        self.log("#{Time.now} == end run ==\n")
+        self.status = "close"
+        self.save      
+      end
+    end
+    
+  end
