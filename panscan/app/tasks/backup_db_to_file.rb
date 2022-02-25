68563d480d9ff1067c79999a7d5c2a8a72f659e3
adam429 <adam429.lee@gmail.com>
2022-02-25 13:54:57 +0800

    task backup

--- panscan/app/tasks/backup_db_to_file.rb
+++ panscan/app/tasks/backup_db_to_file.rb
@@ -1,12 +1,16 @@
-dest_path = "./app/tasks/backup"
-names = Task.where("name is not null").where("tid is not null").where(" now() - save_ti
-mestamp < ? ","1 minutes").map {|x| x.name }.uniq
+loop do
+    dest_path = "./app/tasks/backup"
+    names = Task.where("name is not null").where("tid is not null").where(" now() - save_timestamp < ? ","1 minutes").map {|x| x.name }.uniq
 
-names.each do |name|
-    task = Task.where(name:name).where("tid is not null").order(save_timestamp: :desc).first
-    File.write "#{dest_path}/#{name}.rb", task.code
+    names.each do |name|
+        task = Task.where(name:name).where("tid is not null").order(save_timestamp: :desc).first
+        File.write "#{dest_path}/#{name}.rb", task.code
+    end
+
+    puts `git add *`
+    puts `git commit -m 'task backup'`
+    puts `git push`
+
+    puts "==time: #{time.now.to_s(:db)}=="
+    sleep(60)
 end
-
-`git add *`
-`git commit -m 'task backup'`
-`git push`
