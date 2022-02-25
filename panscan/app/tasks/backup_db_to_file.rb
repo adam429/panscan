807711e17f813bf0c388fdf1e4af8e650f3e3e7b
adam429 <adam429.lee@gmail.com>
2022-02-25 13:22:34 +0800

    task backup

--- panscan/app/tasks/backup_db_to_file.rb
+++ panscan/app/tasks/backup_db_to_file.rb
@@ -1,5 +1,6 @@
 dest_path = "./app/tasks/backup"
-names = Task.where("name is not null").where("tid is not null").map {|x| x.name }.uniq
+names = Task.where("name is not null").where("tid is not null").where(" now() - save_ti
+mestamp < ? ","1 minutes").map {|x| x.name }.uniq
 
 names.each do |name|
     task = Task.where(name:name).where("tid is not null").order(save_timestamp: :desc).first
