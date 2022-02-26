9d434e68a1681f4e67bb548110f7fed5138405bb
adam429 <adam429.lee@gmail.com>
2022-02-26 12:58:31 +0800

    task backup

--- panscan/app/tasks/backup/demo_clean_space.rb
+++ panscan/app/tasks/backup/demo_clean_space.rb
@@ -4,6 +4,7 @@ __TASK_NAME__ = "demo_clean_space"
     saved_methods = self.class.methods
     saved_class_variables = self.class.class_variables
     saved_instance_variables = self.class.instance_variables
+
     saved_instance_methods = self.class.instance_methods
     
 
@@ -14,17 +15,23 @@ __TASK_NAME__ = "demo_clean_space"
     
     module B
     end
-
-    _log(self.class.constants.to_s+"\n")
-
+    
+    def foo
+    end
+    
+    @a=1
+    
+    
 
     self.class.constants.filter {|x| not saved_constants.include?(x) }.map {|x| self.class.send(:remove_const, x); _log x.to_s+"\n"}
+    self.class.methods.filter {|x| not saved_methods.include?(x) }.map {|x| self.class.remove_method(x); _log x.to_s+"\n"}
+    self.class.class_variables.filter {|x| not saved_class_variables.include?(x) }.map {|x| self.class.remove_class_variable(x); _log x.to_s+"\n"}
+    self.class.instance_variables.filter {|x| not saved_instance_methods.include?(x) }.map {|x| self.class.remove_instance_variables(x); _log x.to_s+"\n"}
 
 def main()
     
     # const
-    _log A.class.to_s+"\n" # error
-    _log B.class.to_s+"\n" # error
-
-    _log CONST.to_s+"\n" # error
+    #_log A.class.to_s+"\n" # error
+    #_log B.class.to_s+"\n" # error
+    #_log CONST.to_s+"\n" # error
 end
