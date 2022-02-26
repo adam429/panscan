8e12aae251fdf60cff5e06579f6a0c56f69bb7e9
adam429 <adam429.lee@gmail.com>
2022-02-26 12:41:31 +0800

    task backup

--- panscan/app/tasks/backup/clean space.rb
+++ panscan/app/tasks/backup/clean space.rb
@@ -1,6 +1,16 @@
 __TASK_NAME__ = "clean space"
 
 def main()
-    _log Task.count.to_s + "\n"
-    _log Task.name.to_s + "\n"
+    saved_constants = Object.constants
+    saved_methods = Object.methods
+    saved_class_variables = Object.class_variables
+    saved_instance_variables = Object.instance_variables
+    saved_instance_methods = Object.instance_methods
+    
+    # const
+    Const_A=1
+    
+    Object.constants.filter {|x| not saved_constants.include?(x) }.map {|x| Object.send(:remove_const, x)}
+
+    puts Const_A # error
 end
