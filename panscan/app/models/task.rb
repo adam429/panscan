27d4ca6fd5dc700bb091d91857ec7a3b1b97cfb4
adam429 <adam429.lee@gmail.com>
2022-02-26 14:23:30 +0800

    task backup

--- panscan/app/models/task.rb
+++ panscan/app/models/task.rb
@@ -221,19 +221,12 @@ CODE
         end
 
         code = '''
-        _log("==clean up environment==\n")
-        _log("- constants\n")
-        self.class.constants.filter {|x| not $__saved_constants.include?(x) }.map {|x| _log x.to_s+"\n"; self.class.send(:remove_const, x); }
-        _log("- methods\n")
-        self.class.methods.filter {|x| not $__saved_methods.include?(x) }.map {|x| _log x.to_s+"\n"; eval("class <<#{self.class}\n remove_method :#{x}\n end") }
-        _log("- class_variables\n")
-        self.class.class_variables.filter {|x| not $__saved_class_variables.include?(x) }.map {|x| _log x.to_s+"\n"; self.class.remove_class_variable(x); }
-        _log("- instance_variables\n")
-        self.instance_variables.filter {|x| not $__saved_instance_variables.include?(x) }.map {|x| _log x.to_s+"\n"; remove_instance_variable(x); }
-        _log("- instance_methods\n")
-        self.class.instance_methods.filter {|x| not $__saved_instance_methods.include?(x) }.map {|x| _log x.to_s+"\n"; eval("undef #{x}"); }
-        _log("- global_variables\n")
-        self.global_variables.filter {|x| not $__saved_global_variables.include?(x) }.map {|x| _log x.to_s+"\n"; eval("#{x}=nil"); }
+        self.class.constants.filter {|x| not $__saved_constants.include?(x) }.map {|x| self.class.send(:remove_const, x); }
+        self.class.methods.filter {|x| not $__saved_methods.include?(x) }.map {|x| eval("class <<#{self.class}\n remove_method :#{x}\n end") }
+        self.class.class_variables.filter {|x| not $__saved_class_variables.include?(x) }.map {|x| self.class.remove_class_variable(x); }
+        self.instance_variables.filter {|x| not $__saved_instance_variables.include?(x) }.map {|x| remove_instance_variable(x); }
+        self.class.instance_methods.filter {|x| not $__saved_instance_methods.include?(x) }.map {|x| eval("undef #{x}"); }
+        self.global_variables.filter {|x| not $__saved_global_variables.include?(x) }.map {|x| eval("#{x}=nil"); }
         '''
         eval(code)
         self.log("Exception Class: #{ error.class.name }\n")
