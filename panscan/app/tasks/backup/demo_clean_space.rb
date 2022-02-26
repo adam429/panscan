37ed7eccb7174cf955e6762a2ce8c26b5cd1534b
adam429 <adam429.lee@gmail.com>
2022-02-26 13:19:49 +0800

    task backup

--- panscan/app/tasks/backup/demo_clean_space.rb
+++ panscan/app/tasks/backup/demo_clean_space.rb
@@ -6,7 +6,7 @@ __TASK_NAME__ = "demo_clean_space"
     saved_instance_variables = self.instance_variables
 
     saved_instance_methods = self.class.instance_methods
-    
+    saved_global_variables = self.global_variables
 
     CONST=1
     
@@ -43,6 +43,8 @@ __TASK_NAME__ = "demo_clean_space"
     self.instance_variables.filter {|x| not saved_instance_variables.include?(x) }.map {|x| _log x.to_s+"\n"; remove_instance_variable(x); }
     _log("==instance_methods==\n")
     self.class.instance_methods.filter {|x| not saved_instance_methods.include?(x) }.map {|x| _log x.to_s+"\n"; eval("undef #{x}"); }
+    _log("==global_variables==\n")
+    self.global_variables.filter {|x| not saved_global_variables.include?(x) }.map {|x| _log x.to_s+"\n"; eval("#{x}=nil"); }
 
 def main()
     
