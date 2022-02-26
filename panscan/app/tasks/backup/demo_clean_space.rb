62cfeea5eec260ad2a3eb4a8f56bba1dcdde573e
adam429 <adam429.lee@gmail.com>
2022-02-26 14:05:51 +0800

    task backup

--- panscan/app/tasks/backup/demo_clean_space.rb
+++ panscan/app/tasks/backup/demo_clean_space.rb
@@ -1,11 +1,11 @@
 __TASK_NAME__ = "demo_clean_space"
 
-    $__saved_constants = self.class.constants
-    $__saved_methods = self.class.methods
-    $__saved_class_variables = self.class.class_variables
-    $__saved_instance_variables = self.instance_variables
-    $__saved_instance_methods = self.class.instance_methods
-    $__saved_global_variables = self.global_variables
+    # $__saved_constants = self.class.constants
+    # $__saved_methods = self.class.methods
+    # $__saved_class_variables = self.class.class_variables
+    # $__saved_instance_variables = self.instance_variables
+    # $__saved_instance_methods = self.class.instance_methods
+    # $__saved_global_variables = self.global_variables
 
     CONST=1
     
@@ -34,27 +34,27 @@ __TASK_NAME__ = "demo_clean_space"
     """)
     
     
-    _log("==clean up environment==\n")
-    _log("- constants\n")
-    self.class.constants.filter {|x| not $__saved_constants.include?(x) }.map {|x| _log x.to_s+"\n"; self.class.send(:remove_const, x); }
-    _log("- methods\n")
-    self.class.methods.filter {|x| not $__saved_methods.include?(x) }.map {|x| _log x.to_s+"\n"; eval("class <<#{self.class}\n remove_method :#{x}\n end") }
-    _log("- class_variables\n")
-    self.class.class_variables.filter {|x| not $__saved_class_variables.include?(x) }.map {|x| _log x.to_s+"\n"; self.class.remove_class_variable(x); }
-    _log("- instance_variables\n")
-    self.instance_variables.filter {|x| not $__saved_instance_variables.include?(x) }.map {|x| _log x.to_s+"\n"; remove_instance_variable(x); }
-    _log("- instance_methods\n")
-    self.class.instance_methods.filter {|x| not $__saved_instance_methods.include?(x) }.map {|x| _log x.to_s+"\n"; eval("undef #{x}"); }
-    _log("- global_variables\n")
-    self.global_variables.filter {|x| not $__saved_global_variables.include?(x) }.map {|x| _log x.to_s+"\n"; eval("#{x}=nil"); }
+    # _log("==clean up environment==\n")
+    # _log("- constants\n")
+    # self.class.constants.filter {|x| not $__saved_constants.include?(x) }.map {|x| _log x.to_s+"\n"; self.class.send(:remove_const, x); }
+    # _log("- methods\n")
+    # self.class.methods.filter {|x| not $__saved_methods.include?(x) }.map {|x| _log x.to_s+"\n"; eval("class <<#{self.class}\n remove_method :#{x}\n end") }
+    # _log("- class_variables\n")
+    # self.class.class_variables.filter {|x| not $__saved_class_variables.include?(x) }.map {|x| _log x.to_s+"\n"; self.class.remove_class_variable(x); }
+    # _log("- instance_variables\n")
+    # self.instance_variables.filter {|x| not $__saved_instance_variables.include?(x) }.map {|x| _log x.to_s+"\n"; remove_instance_variable(x); }
+    # _log("- instance_methods\n")
+    # self.class.instance_methods.filter {|x| not $__saved_instance_methods.include?(x) }.map {|x| _log x.to_s+"\n"; eval("undef #{x}"); }
+    # _log("- global_variables\n")
+    # self.global_variables.filter {|x| not $__saved_global_variables.include?(x) }.map {|x| _log x.to_s+"\n"; eval("#{x}=nil"); }
 
 def main()
     
-    # const
-    #_log A.class.to_s+"\n" # error
-    #_log B.class.to_s+"\n" # error
-    #_log CONST.to_s+"\n" # error
-    #_log foo #error
-    #_log @var1.to_s # nil
+    const
+    _log A.class.to_s+"\n" # error
+    _log B.class.to_s+"\n" # error
+    _log CONST.to_s+"\n" # error
+    _log foo #error
+    _log @var1.to_s # nil
     
 end
