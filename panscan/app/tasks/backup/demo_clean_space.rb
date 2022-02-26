a87f99969c070a1cdcf2419fb1c41d6cfbb97697
adam429 <adam429.lee@gmail.com>
2022-02-26 13:32:28 +0800

    task backup

--- panscan/app/tasks/backup/demo_clean_space.rb
+++ panscan/app/tasks/backup/demo_clean_space.rb
@@ -25,7 +25,7 @@ __TASK_NAME__ = "demo_clean_space"
     
     
     
-    _log("""
+    eval("""
     class #{self.class}
       def a
       end
@@ -39,7 +39,7 @@ __TASK_NAME__ = "demo_clean_space"
     _log("==constants==\n")
     self.class.constants.filter {|x| not saved_constants.include?(x) }.map {|x| _log x.to_s+"\n"; self.class.send(:remove_const, x); }
     _log("==methods==\n")
-    self.class.methods.filter {|x| not saved_methods.include?(x) }.map {|x| _log x.to_s+"\n"; eval("class #{self.class}\n remove_method :#{x}\n end") }
+    self.class.methods.filter {|x| not saved_methods.include?(x) }.map {|x| _log x.to_s+"\n"; eval("class <<#{self.class}\n remove_method :#{x}\n end") }
     _log("==class_variables==\n")
     self.class.class_variables.filter {|x| not saved_class_variables.include?(x) }.map {|x| _log x.to_s+"\n"; self.class.remove_class_variable(x); }
     _log("==instance_variables==\n")
