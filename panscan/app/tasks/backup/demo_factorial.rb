9638ebdcf7d51ffc5b35e0af0b856cc7f176ed29
adam429 <adam429.lee@gmail.com>
2022-02-27 16:54:14 +0800

    task backup

--- panscan/app/tasks/backup/demo_factorial.rb
+++ panscan/app/tasks/backup/demo_factorial.rb
@@ -3,10 +3,11 @@ __TASK_NAME__ = "demo_factorial"
 
 def main()
     n = __n__
-    
-    if n==1 then
-        return "1"
-    else
-        return n*(Task.run_remote(demo_factorial,{n:"#{n-1}"})).to_i
-    end
+
+    return Task.run_remote(demo_factorial,{n:"1"})    
+    # if n==1 then
+    #     return "1"
+    # else
+    #     return n*(Task.run_remote(demo_factorial,{n:"#{n-1}"})).to_i
+    # end
 end
