b81b9a1c2155eafb65539327eeb645ec9f3df9bb
adam429 <adam429.lee@gmail.com>
2022-02-27 16:45:57 +0800

    task backup

--- panscan/app/tasks/backup/demo_factorial.rb
+++ panscan/app/tasks/backup/demo_factorial.rb
@@ -1,17 +1,12 @@
 __TASK_NAME__ = "demo_factorial"
 
 
-
 def main()
-    database_init(false) # allow to write
+    n = __n__
     
-    block_numbers = __block_begin__..__block_end__
-    
-    pan_call = PancakePrediction.new
-    
-    Parallel.map(block_numbers.to_a,in_threads: 10) do |block_number|  
-      _log  "#{Time.now} blocks: #{block_number}\n" if block_number%10000==0
-      data_import_block(block_number,pan_call.client,pan_call.address,pan_call.function_abi,pan_call.event_abi,pan_call.decoder)
+    if n==1 then
+        return 1
+    else
+        return n*Task.run_remote(demo_factorial,{n:n-1})    
     end
-    
 end
