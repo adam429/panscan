2db5c39399014cb962264d06a274cef3beb52f1b
adam429 <adam429.lee@gmail.com>
2022-03-03 11:57:45 +0800

    task backup

---
+++ panscan/app/tasks/backup/demo/load/demo_load_class.rb
@@ -0,0 +1,21 @@
+__TASK_NAME__ = "demo/load/demo_load_class"
+
+load(Task.load("demo/load/demo_load_lv2::_mul"))
+
+
+class Avg
+    def self.call(array)
+        array.sum.to_f / array.size
+    end
+    
+end
+
+def mul(a,b)
+    _mul(a,b)
+end
+
+
+def main
+    _log (Avg.call [1,2,3,4]).to_s+"\n"
+    _log mul(2,3).to_s+"\n"
+end
