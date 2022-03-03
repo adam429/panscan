98d0dd1e0cc4124c5c241341c5a828fa185a321e
adam429 <adam429.lee@gmail.com>
2022-03-03 22:43:37 +0800

    task schedule

--- panscan/app/models/address.rb
+++ panscan/app/models/address.rb
@@ -1,3 +1,22 @@
+module AutoRetry
+  def auto_retry(logger=nil,retry_cnt=12)
+      begin
+          yield
+      rescue Faraday::TimeoutError,Net::OpenTimeout,OpenSSL::SSL::SSLError,JSON::ParserError,Net::ReadTimeout=>e
+          if (retry_cnt-=1) > 0 then
+              retry_number = 12-retry_cnt
+              
+              logger.call "sleep #{0.01*(2**retry_number)}" if logger!=nil
+              sleep (0.01*(2**retry_number))
+              logger.call "retry_cnt #{retry_number} at #{Time.now.to_s}" if logger!=nil
+              retry 
+          else
+              raise e
+          end
+      end
+  end
+end
+
 class Address < ApplicationRecord   
     def self.to_graph(addr,level_addr,min_amount=0, max_connect=100,contract=true)
 
@@ -95,11 +114,15 @@ class Address < ApplicationRecord
     end
     
     def self.is_contract(address)
+      Object.include AutoRetry
+
       addr = Address.find_by_addr(address)
       return addr.is_contract if addr
-      return self.client.eth_get_code(address,"latest")["result"]!="0x"
+      return auto_retry() { self.client.eth_get_code(address,"latest")["result"]!="0x" }
     end
+
     
+  
     def decode_params(input)
       if @function_abi==nil and @event_abi==nil then    
         abi_constructor_inputs, abi_functions, abi_events = Ethereum::Abi.parse_abi(abi)
