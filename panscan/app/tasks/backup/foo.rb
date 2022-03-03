4d4df5b319bcd4319b112f9bc4c86085b15dbba7
adam429 <adam429.lee@gmail.com>
2022-03-03 08:53:39 +0800

    task backup

--- panscan/app/tasks/backup/foo.rb
+++ panscan/app/tasks/backup/foo.rb
@@ -1,16 +1,6 @@
 __TASK_NAME__ = "foo"
 
-require 'parallel'
-require 'resolv-replace'
-
-load(Task.load("database"))
-load(Task.load("pancake_prediction"))
-load(Task.load("auto-retry"))
 
 def main
-    class_eval
-    code = "class << Object \n remove_method :class_eval\n end"
-    _log code+"\n"
-   eval(code)
-   
+   sleep(300)
 end
