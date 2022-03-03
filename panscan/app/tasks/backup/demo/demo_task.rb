e52f2ffcd0270a81b9337cde4ff29f09eea84af2
adam429 <adam429.lee@gmail.com>
2022-03-03 12:18:34 +0800

    task backup

--- panscan/app/tasks/backup/demo/demo_task.rb
+++ panscan/app/tasks/backup/demo/demo_task.rb
@@ -1,12 +1,15 @@
 __TASK_NAME__ = "demo/demo_task"
 
-def main()
-    time = Time.now()
-
+def task(count)
     __count__.times do |i|
         _log ("the #{i} run\n")
         sleep(1)
     end
+end
+
+def main()
+    time = Time.now()
+
     time = Time.now()-time
 
     return "takes <span style='color:red'>#{time}</span> seconds"
