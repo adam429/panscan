60df499e3d6a8c450f2ae95579201397a4a5f831
adam429 <adam429.lee@gmail.com>
2022-04-01 23:37:07 +0800

    task backup

--- panscan/app/tasks/backup/panbot/simulation/panbot_simulation_segmentation_interactive.rb
+++ panscan/app/tasks/backup/panbot/simulation/panbot_simulation_segmentation_interactive.rb
@@ -5,7 +5,6 @@ load(Task.load("base/database"))
 load(Task.load("panbot/simulation/panbot_simulation_runner"))
 load(Task.load("panbot/bot/panbot_payout_bot"))
 load(Task.load("panbot/panbot_stats"))
-load(Task.load("base/logger"))
 
 load(Task.load("base/render_wrap"))
 load(Task.load("base/widget"))
