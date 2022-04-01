6f30c8dbc3dca5fd4d96635cf757b6baf0b86107
adam429 <adam429.lee@gmail.com>
2022-04-01 22:49:05 +0800

    task backup

--- panscan/app/tasks/backup/demo/demo_self_task.rb
+++ panscan/app/tasks/backup/demo/demo_self_task.rb
@@ -1,8 +1,7 @@
 __TASK_NAME__ = "demo/demo_self_task"
 
 def main()
-    
-    _log _task.tid.to_s+"\n"
-    _log _task.name.to_s+"\n"
-    _log _task.runner.to_s+"\n"
+    $logger $task.tid
+    $logger $task.name
+    $logger $task.runner
 end
