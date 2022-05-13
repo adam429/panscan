1f05bfc582c567d46af5b570048b3eaa6a245ec5
adam429 <adam429.lee@gmail.com>
2022-05-13 10:49:57 +0800

    task backup

---
+++ panscan/app/tasks/backup/demo/demo_2_load_caller.rb
@@ -0,0 +1,13 @@
+__TASK_NAME__ = "demo/demo_2_load_caller"
+
+
+load(Task.load("demo/load/demo_load_sum::(sum,plus)"))
+load(Task.load("demo/load/demo_load_class"))
+
+def main()
+    $logger.call sum(4,5)
+    $logger.call plus(4,5)
+    $logger.call Avg.call([1,2,3,4])
+    $logger.call mul(2,3)
+    nil
+end
---
+++ panscan/app/tasks/backup/demo/demo_3_schedule_task.rb
@@ -0,0 +1,10 @@
+__TASK_NAME__ = "demo/demo_3_schedule_task"
+__ENV__ = 'ruby3'
+
+$task.next_schedule_at = Time.now+60
+
+def main()
+    sleep(30)
+    time = Time.now()
+    return "#{time} - running"
+end
