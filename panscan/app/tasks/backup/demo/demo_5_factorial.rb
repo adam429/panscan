24f9494c51e589be5d4448cb1ab7dad31189fab0
adam429 <adam429.lee@gmail.com>
2022-05-13 10:56:11 +0800

    task backup

---
+++ panscan/app/tasks/backup/demo/demo_5_factorial.rb
@@ -0,0 +1,13 @@
+__TASK_NAME__ = "demo/demo_5_factorial"
+
+def main()
+    n = __n__
+    
+    if n.to_i==1 then
+        return 1
+    else
+      task =Task.run_remote(_task.name,{n:n-1})
+      update_task = Task.wait_until_done(task)
+      return update_task.raw_ret.to_i * n
+    end
+end
