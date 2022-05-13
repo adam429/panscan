63790531dd0bd99f96f0d572dc6ab82cdd000a2d
adam429 <adam429.lee@gmail.com>
2022-05-13 10:47:52 +0800

    task backup

---
+++ panscan/app/tasks/backup/demo/demo_1_task.rb
@@ -0,0 +1,19 @@
+__TASK_NAME__ = "demo/demo_1_task"
+__ENV__ = 'ruby3'
+
+def task(count)
+    count.times do |i|
+        $logger.call "the #{i} run"
+        sleep(1)
+    end
+end
+
+def main()
+    time = Time.now()
+    
+    task(__count__)
+
+    time = Time.now()-time
+
+    return "takes <span style='color:red'>#{time}</span> seconds"
+end
