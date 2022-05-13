98cfa77fa46881e0b0abebddaf16e074a42f109f
adam429 <adam429.lee@gmail.com>
2022-05-13 10:59:18 +0800

    task backup

---
+++ panscan/app/tasks/backup/demo/demo_5_1_formatter.rb
@@ -0,0 +1,37 @@
+__TASK_NAME__ = "demo/demo_5_1_formatter"
+__ENV__ = 'ruby3'
+
+
+def main()
+    return [1,2,3,4]
+end
+
+def render_html()
+'''<h1>output</h1>
+<% @raw_ret.each do |item| %>
+    <li><%= item %></li>
+<% end %>
+
+<h1>counter</h1>
+<div id="counter"></div>
+'''
+end
+
+def render_js_rb()
+'''
+puts "hello world!"
+
+counter = 0
+
+$$[:setInterval].call(->{ $document.at_css("#counter").inner_html="tick #{counter}"; counter=counter+1 },1000)
+
+
+$timeout = $$[:setTimeout].call(->{
+                puts "timeout hello world"
+              },1000)
+puts $timeout
+$$[:clearTimeout].call($timeout)
+
+'''
+end
+
