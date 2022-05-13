dc1d0bb34892e246fe29649a1c39d7c5a8616874
adam429 <adam429.lee@gmail.com>
2022-05-13 10:57:13 +0800

    task backup

---
+++ panscan/app/tasks/backup/demo/other/demo_auto_split.rb
@@ -0,0 +1,26 @@
+__TASK_NAME__ = "demo/other/demo_auto_split"
+__ENV__ = 'ruby3'
+
+load(Task.load("base/auto_split"))
+
+def main()
+    time = Time.now()
+    
+    begin_param = __begin_param__    
+    end_param = __end_param__    
+    step = 20
+
+    # map stage
+    ret = auto_split_remote_task(begin_param,end_param,step) do |begin_param,end_param|
+        
+        (begin_param..end_param).map {|x| x }.sum
+    end
+    
+    # reduce stage
+    ret = ret.sum if ret.class==Array
+
+    time = Time.now()-time
+    
+    return ret
+
+end
