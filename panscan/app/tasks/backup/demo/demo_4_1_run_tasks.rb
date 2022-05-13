ffec306ae0d153d88eaa968a1ff0b8f6e953c399
adam429 <adam429.lee@gmail.com>
2022-05-13 10:58:16 +0800

    task backup

---
+++ panscan/app/tasks/backup/demo/demo_4_1_run_tasks.rb
@@ -0,0 +1,16 @@
+__TASK_NAME__ = "demo/demo_4_1_run_tasks"
+__ENV__ = 'ruby3'
+
+def main()
+    remote_task = []
+    
+    remote_task << Task.run_remote("demo/demo_1_task",{count:"1"},Time.at(0),$logger)
+    remote_task << Task.run_remote("demo/demo_1_task",{count:"2"},Time.at(0),$logger)
+    remote_task << Task.run_remote("demo/demo_1_task",{count:"3"},Time.at(0),$logger)
+    
+    remote_task = Task.wait_until_done(remote_task)
+    remote_task.each do |task|
+        $logger.call "#{task.output}"
+    end
+    return "done"
+end
