173427a7839a4aae79adffdcdaaf126f5a02d035
adam429 <adam429.lee@gmail.com>
2022-05-19 06:12:24 +0800

    task backup

--- panscan/app/tasks/backup/uniswap/simulation_runner_v7.rb
+++ panscan/app/tasks/backup/uniswap/simulation_runner_v7.rb
@@ -293,8 +293,8 @@ def simulation_runner(pool_id,sim_data,out_of_service=false)
       <%= calculated_var %( $data['sim'].config['sim_time_end'] = :sim_time_end  ) %>
       <%= calculated_var %( $data['sim'].config['price_a_mul'] = :price_a_mul  ) %>
       <%= calculated_var %( $data['sim'].config['price_b_mul'] = :price_b_mul  ) %>
-      <%= calculated_var %( $data['sim'].config['token0'] = :token0  ) %>
-      <%= calculated_var %( $data['sim'].config['token1'] = :token1  ) %>
+      <%= calculated_var %( $data['sim'].config['add_liquidity_token0'] = :add_liquidity_token0  ) %>
+      <%= calculated_var %( $data['sim'].config['add_liquidity_token1'] = :add_liquidity_token1  ) %>
 
       
     EOS
@@ -921,8 +921,8 @@ $document.ready do
     $vars[:sim_time_end] = ($data['sim'].config['sim_time_end'] or $data['sim'].dex.count-1)
     $vars[:price_a_mul] = ($data['sim'].config['price_a_mul'] or -20)
     $vars[:price_b_mul] = ($data['sim'].config['price_b_mul'] or 20)
-    $vars[:token0] = ($data['sim'].config['token0'] or 0)
-    $vars[:token1] = ($data['sim'].config['token1'] or 0)
+    $vars[:add_liquidity_token0] = ($data['sim'].config['add_liquidity_token0'] or 0)
+    $vars[:add_liquidity_token1] = ($data['sim'].config['add_liquidity_token1'] or 0)
 
     $logger.call $data['sim'].bot.config
 
