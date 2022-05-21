5b1cf02ebd9949f103a114195801e06ad4b8d945
adam429 <adam429.lee@gmail.com>
2022-05-21 22:14:35 +0800

    task backup

--- panscan/app/tasks/backup/uniswap/simulation_class_v4.rb
+++ panscan/app/tasks/backup/uniswap/simulation_class_v4.rb
@@ -36,14 +36,13 @@ class Timer
 
             (sim_time_ts..sim_time_end_ts).step(1).each do |ts|
                 if (ts-sim_time_ts) % 3600==0 then
-                    sim_status = "#{Time.now} : Simulation Progress [#{ts-sim_time_ts} / #{sim_time_end-sim_time}] : #{ObjectSpace.memsize_of_all/1_000_000} MB memory"
+                    sim_status = "#{Time.now} : Simulation Progress [#{ts-sim_time_ts} / #{sim_time_end_ts-sim_time_ts}] : #{ObjectSpace.memsize_of_all/1_000_000} MB memory"
                     $logger.call(sim_status)
                 end
-                ts = @sim.time_table.find_ts_by_id(id)
                 yield(ts)
             end
 
-            sim_status = "#{Time.now} : Simulation Progress [#{sim_time_end-sim_time} / #{sim_time_end-sim_time}] : #{ObjectSpace.memsize_of_all/1_000_000} MB memory"
+            sim_status = "#{Time.now} : Simulation Progress [#{sim_time_end_ts-sim_time_ts} / #{sim_time_end_ts-sim_time_ts}] : #{ObjectSpace.memsize_of_all/1_000_000} MB memory"
             $logger.call(sim_status)
         end
 
